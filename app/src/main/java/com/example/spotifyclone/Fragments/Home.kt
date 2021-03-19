e99b0ed26bcdde1da202a38e2b5bf881d46a8d60
luciano <lopezz.lucio85@gmail.com>
2021-03-19 10:26:24 -0300

    lista horizontal(local)

--- app/src/main/java/com/example/spotifyclone/Fragments/Home.kt
+++ app/src/main/java/com/example/spotifyclone/Fragments/Home.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.spotifyclone.Model.Album
 import com.example.spotifyclone.Model.Categoria
 import com.example.spotifyclone.R
+import kotlinx.android.synthetic.main.album_item.view.*
 import kotlinx.android.synthetic.main.categoria_item.view.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -16,8 +18,8 @@ class Home : Fragment() {
 
     private lateinit var categoriaAdapter: CategoriaAdapter
 
-    companion object{
-        fun newInstance(): Home{
+    companion object {
+        fun newInstance(): Home {
             val fragmentHome = Home()
             val argumentos = Bundle()
             fragmentHome.arguments = argumentos
@@ -28,8 +30,8 @@ class Home : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -38,10 +40,17 @@ class Home : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         val categorias: MutableList<Categoria> = ArrayList()
-        for (c in 0..4){
+        for (c in 0..4) {
             val categoria = Categoria()
             categoria.titulo = "Categoria$c"
 
+            val albuns: MutableList<Album> = ArrayList()
+            for(a in 0..19){
+                val album = Album()
+                album.album = R.drawable.spotify
+                albuns.add(album)
+            }
+            categoria.albuns = albuns
             categorias.add(categoria)
         }
 
@@ -50,10 +59,11 @@ class Home : Fragment() {
         recycler_view_categoria.layoutManager = LinearLayoutManager(context)
 
     }
-    private inner class CategoriaAdapter(internal val categorias: MutableList<Categoria>): RecyclerView.Adapter<CategoriaHolder>() {
+
+    private inner class CategoriaAdapter(internal val categorias: MutableList<Categoria>) : RecyclerView.Adapter<CategoriaHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaHolder {
-            return CategoriaHolder(layoutInflater.inflate(R.layout.categoria_item,parent,false))
+            return CategoriaHolder(layoutInflater.inflate(R.layout.categoria_item, parent, false))
         }
 
         override fun getItemCount(): Int = categorias.size
@@ -65,18 +75,40 @@ class Home : Fragment() {
             holder.bind(categoria)
         }
     }
-    private inner class CategoriaHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        fun bind(categoria: Categoria){
+
+    private inner class CategoriaHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(categoria: Categoria) {
             itemView.text_titulo_categoria.text = categoria.titulo
-//            itemView.recycler_listagem_albuns.adapter = AlbunsAdapter(categoria.albuns){album ->
-//
-//                val intent = Intent(context,Detalhes::class.java)
-//                intent.putExtra("album",album.album)
-//                intent.putExtra("titulos", titulos[album.id])
-//                startActivity(intent)
-//
-//            }
-//            itemView.recycler_albuns.layoutManager = LinearLayoutManager(context,RecyclerView.HORIZONTAL,false)
+            itemView.recycler_listagem_albuns.adapter = AlbunsAdapter(categoria.albuns)
+            itemView.recycler_listagem_albuns.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
         }
+    }
+
+
+
+
+    //========================= Albuns Horizontal ================================
+
+    private inner class AlbunsAdapter(private val albuns: MutableList<Album>): RecyclerView.Adapter<AlbunsHolder>() {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbunsHolder {
+            return AlbunsHolder(layoutInflater.inflate(R.layout.album_item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: AlbunsHolder, position: Int) {
+            val album = albuns[position]
+            holder.bind(album)
+        }
+
+        override fun getItemCount(): Int = albuns.size
+
+        }
+    }
+
+    private class AlbunsHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(album: Album){
+            itemView.image_album.setImageResource(album.album)
     }
 }
--- app/src/main/java/com/example/spotifyclone/Model/Dados.kt
+++ app/src/main/java/com/example/spotifyclone/Model/Dados.kt
@@ -1,5 +1,10 @@
 package com.example.spotifyclone.Model
 
 class Categoria(
-        var titulo: String = ""
+        var titulo: String = "",
+        var albuns: MutableList<Album> = ArrayList()
+)
+
+class Album(
+        var album: Int = 0
 )
